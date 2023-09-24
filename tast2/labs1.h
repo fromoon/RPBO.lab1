af2571b39953b5a1b4b9018322c2428c43ecd131
zeq <75920759+fromoon@users.noreply.github.com>
2023-09-25 00:05:05 +0300

    Add files via upload

---
+++ tast2/labs1.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+unsigned short ReadPersonAge();
+string ReadPersonName();
+void ReadPersonHeight();
+void ReadPersonWeight(unsigned short& weight);
+void ReadPersonSalary(double* salary);
+void ReadPersonData(string& names, unsigned short& ages, double& salaries);
+void ReadPersonData(string& names, unsigned short& ages, unsigned short& heights, unsigned short& weigths);
+void WritePersonData(const string& names, const unsigned short* ages, const string& heights, const string& weigths, string salaries);
